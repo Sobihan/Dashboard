3998885ec5552b06821fbee46bc47c7110d96fd3
Amaya <guillaume.le-berre@epitech.eu>
2020-11-17 13:50:54 +0100

    [UP] basic errors

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -6,10 +6,36 @@
 */
 
 #include <iostream>
+#include <fstream>
 
-int main()
+bool is_digit(const std::string &str)
 {
-    std::cout << "Success" << std::endl;
+    for (const char c : str)
+        if (c < '0' || c > '9')
+            return false;
+    return true;
+}
 
+void help()
+{
+    std::cerr << "USAGE : ./dashboard_server port\n";
+    std::cerr << "\tport:\tValid port, only numeric characters, 4 char long\n";
+}
+
+int main(int ac, char **av)
+{
+    if (ac != 2) {
+        help();
+        return 84;
+    }
+
+    std::string port = av[1];
+
+    if (port.length() != 4 || !is_digit(port)) {
+        help();
+        return 84;
+    }
+
+    std::cout << "Success" << std::endl;
     return 0;
 }
