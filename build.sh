959c521ed335cca76c6b34b08b630869148dd9e6
Amaya <guillaume.le-berre@epitech.eu>
2020-11-17 13:42:45 +0100

    [INIT] server

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@
 
 mkdir -p build
 cd build
+conan install ..
+cmake .. -G "Unix Makefiles"
+cmake --build .
---
+++ server/src/main.cpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2020
+** B-DEV-500-PAR-5-1-cardgames-guillaume.le-berre
+** File description:
+** main
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Success" << std::endl;
+
+    return 0;
+}
